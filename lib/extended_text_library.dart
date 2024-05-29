23adfd6237a828ae7614bf9f96eb1698d1b1eb22
hubaoqi <1006462702@qq.com>
2024-05-29 16:07:32 +0800

    fix：修复 输入框仅复制文件的时候复制失败

--- lib/extended_text_library.dart
+++ lib/extended_text_library.dart
@@ -5,6 +5,7 @@ export 'src/extended_text_typedef.dart';
 export 'src/extended_text_utils.dart';
 export 'src/extended_widget_span.dart';
 export 'src/image_span.dart';
+export 'src/file_span.dart';
 export 'src/special_inline_span_base.dart';
 export 'src/special_text_span.dart';
 export 'src/special_text_span_builder.dart';
---
+++ lib/src/file_span.dart
@@ -0,0 +1,98 @@
+import 'dart:ui' as ui show PlaceholderAlignment;
+
+import 'package:flutter/material.dart';
+
+import 'extended_widget_span.dart';
+
+class FileSpan extends ExtendedWidgetSpan {
+
+  FileSpan({
+    required double fileWidth,
+    required double fileHeight,
+    required String fileName,
+    required String fileSize,
+    required String filePic,
+    required String picPackageSource,
+    EdgeInsets? margin,
+    int start = 0,
+    ui.PlaceholderAlignment alignment = ui.PlaceholderAlignment.middle,
+    String? actualText,
+    TextBaseline? baseline,
+    GestureTapCallback? onTap,
+    HitTestBehavior behavior = HitTestBehavior.deferToChild,
+    MouseCursor cursor = SystemMouseCursors.click,
+  })  : width = fileWidth + (margin == null ? 0 : margin.horizontal),
+        height = fileHeight + (margin == null ? 0 : margin.vertical),
+        super(
+        child: MouseRegion(
+          cursor: cursor,
+          child: Container(
+            padding: margin,
+            child: GestureDetector(
+              onTap: onTap,
+              behavior: behavior,
+              child: Container(
+                width: fileWidth,
+                height: fileHeight,
+                padding: const EdgeInsets.fromLTRB(16, 10, 16, 10),
+                decoration: BoxDecoration(
+                  color: const Color(0xFFFFFFFF),
+                  border: Border.all(
+                    color: const Color(0xFFD0D0D0),
+                  ),
+                ),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            fileName,
+                            maxLines: 1,
+                            style: const TextStyle(
+                              color: Color(0xFF1E1E1E),
+                              fontSize: 14,
+                            ),
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(
+                              top: 4.0,
+                              bottom: 4.0,
+                            ),
+                            child: Text(
+                              fileSize,
+                              style: const TextStyle(
+                                color: Color(0xFFAAAAAA),
+                                fontSize: 12,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(width: 15),
+                    Image.asset(
+                      filePic,
+                      package: picPackageSource,
+                      height: 40,
+                      width: 40,
+                      fit: BoxFit.fill,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+        baseline: baseline,
+        alignment: alignment,
+        start: start,
+        deleteAll: true,
+        actualText: actualText,
+      );
+  final double width;
+  final double height;
+}
